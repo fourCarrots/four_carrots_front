414efd877c8af46d61fef4da37d6483c144a6fcc
ktkdgh <ktkdgh821@gmail.com>
2022-09-12 02:42:31 +0900

    test ci/cd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:four_carrots_front/main.dart';
+// import 'package:four_carrots_front/main.dart';
 
 void main() {
   testWidgets('api smoke test', (WidgetTester tester) async {
